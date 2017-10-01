f9d82d29b758284730903d3ff3013f8638796bf1
Mark O Connor <markanthonyoconnor@gmail.com>
2017-10-01 21:39:04 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log('Starting app.');
+
+const fs = require('fs');
+const _= require('lodash');
+
+const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+
+}else if (command === 'list') {
+    console.log('listing all notes');
+}
+else if (command === 'read') {
+    console.log('Fetching note');
+}
+else if (command === 'remove') {
+    console.log('Removing note');
+}
+else{
+    console.log('Command not recognised');
+}
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('Starting notes app!');
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New Notes';
+};
