e1e7fe211984527c7ca9a4908d29ac507e4ed9c0
Mark O Connor <markanthonyoconnor@gmail.com>
2017-10-03 21:12:44 +0100

    Refactor code

--- notes.js
+++ notes.js
@@ -1,7 +1,33 @@
 console.log('Starting notes app!');
 
+const fs = require('fs');
+
+var fetchNotes = () =>{
+    try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+       
+   } catch (error) {
+       return [];
+   }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
+
 var addNote = (title, body) => {
-    console.log("Adding note", title,body);
+   var notes = fetchNotes();
+   var note ={
+       title,
+       body
+   };
+   var duplicateNotes = notes.filter((note) =>  note.title === title);
+   if (duplicateNotes.length ===0) {
+       notes.push(note);
+       saveNotes(notes);
+   }
 };
 
 var getAll = () =>{
