f3aed5c8738f0f03cc90e58fb1237424d1bb3caa
Mark O Connor <markanthonyoconnor@gmail.com>
2017-12-21 07:37:05 -0600

    arrow funtions examples

--- playground/arrorw-function.js
+++ playground/arrorw-function.js
@@ -1,3 +1,27 @@
-var square = (x) => x *x;
+//Expression Syntax below
+var square = (x) => x * x;
 console.log(square(9));
 
+//statement syntax
+var square1 = (x) =>{
+    var result = x *x;
+    return result;
+};
+
+console.log(square1(4));
+
+var user ={
+    name: "Mark",
+    sayHi: () => {
+        console.log(arguments);// global arguments variable
+        console.log(`Hi I'm  ${this.name}`); //this keyword only works on object literals, 
+
+    },
+    sayHiAlt () {
+        console.log(arguments);
+        console.log(`Hi I'm  ${this.name}`); // When you're creating functions on object literals you use this syntax so you can use the this keyword to bind 
+    }
+};
+
+user.sayHi(1,2,3);
+user.sayHiAlt(1,2,3);
